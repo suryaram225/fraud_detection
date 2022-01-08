f5464dd276f6009eace43abc03737e625106c1c4
Surya Ganesan <suryaram.ganesan@fedex.com>
2022-01-07 20:16:50 -0600

    added a new function sum

---
+++ data_processing.py
@@ -0,0 +1,10 @@
+
+print("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+def sum(a,b):
+    c=a+b
+    return a
+
+print(" added a new function sum which is not correct")
